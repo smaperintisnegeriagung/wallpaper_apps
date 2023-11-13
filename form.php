3471550d8bfc3da11171c149d40461078ea257d7
smaperintisnegeriagung <91403735+smaperintisnegeriagung@users.noreply.github.com>
2023-11-13 20:42:53 +0700

    form.php

--- form.php
+++ form.php
@@ -1,1 +1,23 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Form Sederhana</title>
+</head>
+<body>
+    <form>
+        <div>
+            <label>Nama</label> <br>
+            <input name="nama" type="text" placeholder="Masukkan nama">
+        </div>
+        <div>
+            <label>Alamat</label> <br>
+            <input name="alamat" type="text" placeholder="Masukkan alamat">
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>
+</body>
+</html>
